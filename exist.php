585eb2fac257c240da0250a534dcf26fb0293b04
Kylianjaaay <140892672+Kylianjaaay@users.noreply.github.com>
2023-08-17 19:56:09 +0300

    Add files via upload

---
+++ exist.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>username_existance</title>
+<link href="css/layout.css" rel="stylesheet" type="text/css" />
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="Container">
+<div id="Header"></div>
+<div id="NavBar">
+	<nav>
+    	<ul>
+          	<li><a href="./login.php">Login</a></li>
+            <li><a href="./services.php"> Services</a></li>
+            <li><a href="./register.php">Register</a></li>
+            <li><a href="./admin.php">Admin</a></li>
+            <li><a href="./logout.php">Logout</a></li>
+        </ul>
+    </nav>
+
+</div>
+<div id="Content">
+	<div id="PageHeading"></div>
+	<div id="ContentLeft"></div>
+    <div id="ContentRight"><font color="red">Sorry the UserName is already in the database please go back and re-register!</font></div>
+</div>
+<div id="Footer"></div>
+</div>
+</body>
+</html>
